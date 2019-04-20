d562623788cbc19590a29ab5b2009a4b41779437
Dimitri C <couwenberg@gmail.com>
2019-04-20 13:44:57 +1000

    deleted files

--- examples/fieldArrays/src/calculationHelper.js
+++ examples/fieldArrays/src/calculationHelper.js
@@ -1,21 +1,6 @@
 export function CalcTotal(val)
 {
-//todo create calc
 
-//const items = [{amount1: '100', amount2: '50', name: 'Ruud'}, {amount1: '40', amount2: '60', name: 'Ted'}];
-// let val = [{
-//     currentYear: '11',
-//     lastYear: '12',
-//     name: 'Bert',
-//     type: '+'
-//   },
-//   {
-//     currentYear: '2',
-//     lastYear: '4',
-//     name: 'Ed',
-//     type: '-'
-//   }
-// ];
 const total = val.reduce((a, { currentYear, lastYear, type }) => {
   const mult = type === '+' ? 1 : -1;
   a.currentYear += mult * currentYear;
